e6a1750a794c1877cb4f40301bbc2f3afe2cfcc7
Thol Chidambaram <tholkappia@gmail.com>
2020-04-27 03:01:23 -0700

    added scantron submission and processing of results

--- bin/app.py
+++ bin/app.py
@@ -130,6 +130,13 @@ class NewTestSchema(Schema):
     answer_keys  = fields.Nested(AnswerKeysSchema, Required=True)
     submissions  = fields.List(fields.Nested(ScantronSchema))
 
+class NewAnswerSchema(Schema):
+    scantron_id  = fields.Integer()
+    name         = fields.Str(Required=True)
+    subject      = fields.Str(Required=True)
+    answers      = fields.Nested(AnswerKeysSchema, Required=True)
+
+
 def createDbObjects():
     conn = sqlite3.connect('scantron.db')
     c = conn.cursor()
@@ -149,8 +156,11 @@ def createDbObjects():
 
     c.execute('''CREATE TABLE if not exists submissions
                  (id INTEGER PRIMARY KEY, 
-                 test_id INTEGER, 
-                 answer_image BLOB)''')
+                 test_id INTEGER,
+                 name TEXT,
+                 answer_image BLOB,
+                 score INTEGER)''')
+    c.execute('''CREATE UNIQUE INDEX if not exists submission_name_unq ON submissions(name)''')
 
     c.execute('''CREATE TABLE if not exists submission_answer_keys
                  (submission_id INTEGER, 
@@ -168,7 +178,7 @@ def not_found(error):
 
 @app.errorhandler(400)
 def bad_request(error):
-    return make_response(jsonify({'error': 'Bad Request'}), 400)
+    return make_response(jsonify({'error': 'Bad Request1'}), 400)
 
 @app.route('/api/tests', methods=['POST'])
 def new_test():
@@ -196,134 +206,108 @@ def new_test():
     conn.commit()
     return jsonify(result)
 
+def test_id_exists(test_id):
+    conn = sqlite3.connect('scantron.db')
+    c = conn.cursor()
+    query_test_id = '''SELECT count(*) FROM tests where id = ?'''
+    query_tuple = (test_id,)
+    cursor = c.execute(query_test_id,query_tuple)
+    for row in cursor:
+        count = row[0]
+    if count > 0:
+        return True
+    else:
+        return False
+
+def build_submission_result(submission_id):
+    result = {}
+    result['scantron_id'] = submission_id
+    result['scantron_url'] = "http://localhost:5000/files/scantron-"+ str(submission_id) +".json"
+
+    conn = sqlite3.connect('scantron.db')
+    c = conn.cursor()
+    query_result = '''
+        select s.name, t.subject, s.score, tak.question_number, tak.answer_key, sak.answer_key
+        from test_answer_keys tak, submission_answer_keys sak, submissions s, tests t
+        where tak.question_number = sak.question_number
+        and sak.submission_id = s.id
+        and s.test_id = tak.test_id
+        and s.id = ?
+        and s.test_id = t.id
+    '''
+    query_tuple = (submission_id,)
+    cursor = c.execute(query_result,query_tuple)
+    for row in cursor:
+        result['name'] = row[0]
+        result['subject'] = row[1]
+        result['score'] = row[2]
+        q_no = row[3]
+        result[q_no] = {}
+        result[q_no]['actual'] = row[4]
+        result[q_no]['expected'] = row[5]
+
+    return result
 
 
+def process_scantron_file(test_id, answer_json, answer_str):
+    answer_blob = sqlite3.Binary(json.dumps(answer_str).encode('utf-8'))
+    conn = sqlite3.connect('scantron.db')
+    c = conn.cursor()
+    insert_submission = '''INSERT INTO submissions (test_id, name, answer_image) VALUES (?,?, ?)'''
+    submission_tuple = (test_id, answer_json['name'], answer_blob)
+    c.execute(insert_submission, submission_tuple)
+    submission_id = c.lastrowid
 
-students = [
-    {
-        'id': 1,
-        'name': u'John Doe',
-        'classes': [1,2,3,4], 
-    },
-    {
-        'id': 2,
-        'name': u'Jane Doe',
-        'classes': [2,4], 
-    }
-]
-classes = [
-    {
-        'id':1,
-        'name': 'CMPE-273',
-        'students' : [1]
-    },
-    {
-        'id':2,
-        'name': 'CMPE-281',
-        'students' : [1,2]
-    },
-    {
-        'id':3,
-        'name': 'CMPE-280',
-        'students' : [1]
-    },
-    {
-        'id':4,
-        'name': 'CMPE-272',
-        'students' : [1,2]
-    },
-]
+    insert_submission_answers = '''INSERT INTO submission_answer_keys(submission_id,question_number,answer_key) VALUES (?,?,?)'''
+    for question in answer_json['answers']:
+        c.execute(insert_submission_answers, (submission_id, question, answer_json['answers'][question]))
+
+    get_score = '''
+    select count(*)
+    from test_answer_keys tak, submission_answer_keys sak, submissions s
+    where tak.question_number = sak.question_number
+    and tak.answer_key = sak.answer_key
+    and sak.submission_id = s.id
+    and s.test_id = tak.test_id
+    and s.id = ?
+    '''
+    get_score_tuple = (submission_id,)
+    cursor = c.execute(get_score, get_score_tuple)
+    for row in cursor:
+        count = row[0]
+
+    update_score = '''UPDATE submissions SET score = ? WHERE id = ?'''
+    update_score_tuple = (count, submission_id)
+    c.execute(update_score, update_score_tuple)
+    conn.commit()
+
+    return build_submission_result(submission_id)
 
 
-
-@app.route('/hello')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
-
-@app.route('/students', methods=['GET'])
-def get_students():
-    c = []
-    for s in students:
-        c.append({'name' : s['name']})
-    return jsonify({'students': c})
-
-@app.route('/student_classes', methods=['GET'])
-def get_student_classes():
-    stu_list = copy.deepcopy(students)
-    for s in stu_list:
-        c = []
-        for id in s['classes']:
-            # print(id)
-            for cl in classes:
-                if id == cl['id']:
-                    c.append(cl)
-        s['classes'] = c
-    return jsonify({'students': stu_list})
-
-@app.route('/classes', methods=['GET'])
-def get_classes():
-    c = []
-    for cl in classes:
-        c.append({'name' : cl['name']})
-    return jsonify({'classes': c})
-
-@app.route('/class_students', methods=['GET'])
-def get_class_students():
-    class_list = copy.deepcopy(classes)
-    for c in class_list:
-        s = []
-        for id in c['students']:
-            for st in students:
-                if id == st['id']:
-                    s.append(st)
-        c['students'] = s
-    return jsonify({'students': class_list})
-
-@app.route('/student/<int:student_id>', methods=['GET'])
-def get_student_from_id(student_id):
-    s = [s for s in students if s['id'] == student_id]
-    if len(s) == 0:
-        abort(404)
-    return jsonify({'student': s[0]['name']})
-
-@app.route('/class/<int:class_id>', methods=['GET'])
-def get_class_from_id(class_id):
-    s = [s for s in classes if s['id'] == class_id]
-    if len(s) == 0:
-        abort(404)
-    return jsonify({'class': s[0]['name']})
-
-@app.route('/classes', methods=['POST'])
-def new_class():
-    if not request.json or not 'name' in request.json:
-        abort(400)
-    s = {
-        'id': classes[-1]['id'] + 1,
-        'name' : request.json['name'],
-        'students' : request.json['students']
-    }
-    classes.append(s)
-    return jsonify({'class': s})
-
-@app.route('/class/<int:class_id>', methods=['PATCH'])
-def add_student_to_class(class_id):
-    if not request.json or not 'student_id' in request.json:
-        abort(400)
-    
-    s = [s for s in classes if s['id'] == class_id]
-    if len(s) == 0:
+@app.route('/api/tests/<int:test_id>/scantrons', methods=['POST'])
+def new_scantron(test_id):
+    # print('request.method', request.method)
+    # print('request.args', request.args)
+    # print('request.form', request.form)
+    # print('request.files', request.files)
+    # 'file' not in .. abort doesn't work PAINFUL only IN works (may be due to reuse of 'result')
+    if 'file' in request.files:
+        answer_str = str(request.files['file'].read(),'utf-8')
+        # print(answer_str)
+        answer_json = json.loads(answer_str)
+        try:
+            answer_obj = NewAnswerSchema().load(answer_json)
+        except ValidationError as err:
+            return jsonify({'err_messages' : err.messages, 'err_valid_data' : err.valid_data})
+    else:
         abort(404)
 
-    s[0]['students'].append(request.json['student_id'])
+    if (test_id_exists(test_id)):
+        submission_result = process_scantron_file(test_id, answer_json, answer_str)
+    else:
+        abort(404)
 
-    cl = copy.deepcopy(s[0])
-    c = []
-    for id in cl['students']:
-        # print(id)
-        for st in students:
-            if id == st['id']:
-                c.append(st)
-    cl['students'] = c
+    # return jsonify({"test_id": test_id, "submission_id" : submission_id})
+    return jsonify(submission_result)
 
-    return jsonify(cl), 201
+
